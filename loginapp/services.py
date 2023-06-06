9f58746b4b946a370f7b9ab035df00a14b81738b
Your Name <Studerzhan@gmail.com>
2023-06-06 10:14:01 +0600

    ffe

--- loginapp/services.py
+++ loginapp/services.py
@@ -32,7 +32,7 @@ def check_status(username):
 
 def generate_qr():
     code = set_url()
-    url = f'{HOSTNAME}/{code}/'
+    url = f'{HOSTNAME}/check/{code}/'
     img = qrcode.make(url)
     filename = f'{datetime.now().date()}.png'
     img.save(os.path.join(settings.MEDIA_ROOT, 'qr', filename))
@@ -45,6 +45,6 @@ def clearMedia():
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(generate_qr, 'interval', seconds=17)
+scheduler.add_job(generate_qr, 'interval', seconds=27)
 scheduler.add_job(clearMedia, 'cron', hour=0)
 scheduler.start()
