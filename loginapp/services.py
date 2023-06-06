51f25c04897a68795b22c02ab7b26a878a6f7755
Tukayev_Damir <tukaevdamirg@gmail.com>
2023-06-06 06:52:04 +0600

    очистил файл views

--- loginapp/services.py
+++ loginapp/services.py
@@ -10,6 +10,19 @@ from django.core.cache import cache
 from .models import User
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+import logging
+from django.http import HttpResponse, HttpResponseNotFound
+from django.template import loader
+from .models import Visit
+from openpyxl import Workbook
+from django.db.models import Q
+from io import BytesIO
+
+LOGIN_TEMPLATE = 'loginapp/login.html'
+
+logger = logging.getLogger(__name__)
 
 
 def set_url():
@@ -43,8 +56,126 @@ def clearMedia():
     for image in os.listdir(MEDIA_ROOT):
         os.remove(os.path.join(MEDIA_ROOT + '/qr/', image))
 
+def superuser_required(view_func):
+    code = cache.get('code')
+
+    actual_decorator = user_passes_test(
+        lambda u: u.is_superuser,
+        login_url=f'/check/{code}'
+    )
+    return actual_decorator(view_func)    
+
+def check_authentication(request):
+    if request.user.is_authenticated:
+        if check_status(request.user.username):
+            return redirect(reverse('admin'))
+        else:
+            return redirect(reverse('home'))
+    return render(request, LOGIN_TEMPLATE)   
+
+def handle_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+
+        if check_status(username):
+            return redirect(reverse('admin'))
+        else:
+            return redirect(reverse('home'))
+    else:
+        logger.error(f"Invalid login attempt for username: {username}")
+        return HttpResponse("Invalid login.") 
+
+def show_login_form(request):
+    return check_authentication(request)
+
+def show_qr_scan_again(request, code):
+    message = f"Отсканируйте QR снова"
+    template = loader.get_template('loginapp/success.html')
+    url = code
+    context = {
+        'message': message,
+        'url': url
+    }
+    return HttpResponse(template.render(context, request))
+
+def handle_user_login(user, username, current_date):
+    visit = Visit.objects.create(user=user)
+    visit.save()
+    message = f"Пользователь {username}, сегодня авторизовался"
+    template = loader.get_template('loginapp/success.html')
+    context = {'message': message}
+    return HttpResponse(template.render(context, request))
+
+def handle_user_logout(visit, username):
+    visit.leaving_time = datetime.now().time()
+    visit.save()
+    message = f"Пользователь {username}, успешно вышел"
+    template = loader.get_template('loginapp/success.html')
+    context = {'message': message}
+    return HttpResponse(template.render(context, request))
+
+def get_filtered_visits(request, search_query, since_date_filter, before_date_filter, sort_by):
+    visits = Visit.objects.all()
+    visits = visits.order_by('-date')
+
+    if sort_by == 'first_name':
+        visits = visits.order_by('user__first_name')
+    elif sort_by == 'last_name':
+        visits = visits.order_by('user__last_name')
+    elif sort_by == 'date':
+        visits = visits.order_by('date')
+    elif sort_by == 'arrival_time':
+        visits = visits.order_by('arrival_time')
+    elif sort_by == 'living_time':
+        visits = visits.order_by('leaving_time')
+    elif sort_by == 'working_time':
+        visits = visits.order_by('working_time')
+
+    if search_query:
+        visits = visits.filter(
+            Q(user__first_name__icontains=search_query) |
+            Q(user__last_name__icontains=search_query) |
+            Q(date__icontains=search_query)
+        )
+
+    if since_date_filter:
+        visits = visits.filter(date__gte=since_date_filter)
+        if before_date_filter:
+            visits = visits.filter(date__gte=since_date_filter, date__lte=before_date_filter)
+        else:
+            visits = visits.filter(date=since_date_filter)
+    else:
+        if before_date_filter:
+            visits = visits.filter(date__lte=before_date_filter)
+
+    return visits
+
+def download_visits_report(visits, search_query):
+    # Создаем новый файл Excel и добавляем данные
+    wb = Workbook()
+    ws = wb.active
+    headers = ['Фамилия', 'Имя', 'Время прибытия', 'Время ухода', 'Время работы', 'Время опоздания', 'Переработки']
+    ws.append(headers)
+    for obj in visits:
+        row = [obj.user.last_name, obj.user.first_name, obj.arrival_time, obj.leaving_time, obj.working_time,
+               obj.lateness, obj.recycling]
+        ws.append(row)
+
+    # Генерируем файл Excel в памяти и создаем HTTP-ответ для скачивания файла
+    file_stream = BytesIO()
+    wb.save(file_stream)
+    file_stream.seek(0)
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=report.xlsx'
+    response.write(file_stream.read())
+    return response
+
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(generate_qr, 'interval', seconds=17)
+scheduler.add_job(generate_qr, 'interval', seconds=60)
 scheduler.add_job(clearMedia, 'cron', hour=0)
 scheduler.start()
--- loginapp/views.py
+++ loginapp/views.py
@@ -3,72 +3,25 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import user_passes_test
 from .models import Visit
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseNotFound
 from datetime import datetime, timedelta, date
-from .services import generate_qr, get_cookie, check_status
+from .services import check_status, superuser_required, handle_login, show_login_form, show_qr_scan_again, handle_user_login, handle_user_logout, get_filtered_visits, download_visits_report
 from django.urls import reverse
 from django.core.cache import cache
 from django.template import loader
-from django.db.models import Q
 from .forms import UserFilterForm
-from openpyxl import Workbook
-from io import BytesIO
-
-
-def superuser_required(view_func):
-    code = cache.get('code')
-
-    actual_decorator = user_passes_test(
-        lambda u: u.is_superuser,
-        login_url=f'/check/{code}'
-    )
-    return actual_decorator(view_func)
-
 
 def base(request):
     return render(request, "loginapp/base.html")
 
-
 def index(request, secret_key):
     code = cache.get('code')
     if secret_key == str(code):
-        if request.user.is_authenticated == False:
-            if request.method == 'POST':
-                username = request.POST['username']
-                password = request.POST['password']
-                user = authenticate(request, username=username, password=password)
-                if user is not None:
-                    login(request, user)
-                    expiration_date = datetime.now() + timedelta(days=7)
-
-                    if check_status(username):
-                        response = redirect(reverse('admin'))
-                    else:
-                        response = redirect(reverse('home'))
-                    response.set_cookie('username', username, expires=expiration_date)
-                    return response
-                else:
-                    # Return an 'invalid login' error message.
-                    return HttpResponse("Invalid login.")
-
-                # If request method is GET, show the login form
-
+        if request.method == 'POST':
+            return handle_login(request)
         else:
-            if check_status:
-                return redirect(reverse('admin'))
-            else:
-                return redirect(reverse('home'))
-        return render(request, 'loginapp/login.html')
-    message = f"Отсканируйте QR снова"
-    template = loader.get_template('loginapp/success.html')
-    url = code
-    context = {
-        'message': message,
-        'url': url
-    }
-
-    return HttpResponse(template.render(context, request))
-
+            return show_login_form(request)
+    else:
+        return show_qr_scan_again(request, code)
 
 def home(request):
     user_id = request.user.id
@@ -77,25 +30,15 @@ def home(request):
     current_date = date.today()
     todayVisit = Visit.objects.filter(Q(user=user) & Q(date=current_date))
     if todayVisit.exists() == False:
-        visit = Visit.objects.create(user=user)
-        visit.save()
-        message = f"Пользователь {username}, сегодня авторизовался"
+        return handle_user_login(user, username, current_date)
     else:
-        visit = todayVisit[0]
-        visit.leaving_time = datetime.now().time()
-        visit.save()
-        message = f"Пользователь {username}, успешно вышел"
-    template = loader.get_template('loginapp/success.html')
-    context = {'message': message}
-    return HttpResponse(template.render(context, request))
-
+        return handle_user_logout(todayVisit[0], username)
 
 @user_passes_test(lambda u: u.is_superuser)
 def qr(request):
     date = datetime.now().date()
     image = f'{date}.png'
     return render(request, 'loginapp/qr.html', {'image': image})
-
 
 @superuser_required
 def ceramic_admin_view(request):
@@ -107,69 +50,10 @@ def ceramic_admin_view(request):
     before_date_filter = request.GET.get('date2', '')
     sort_by = request.GET.get('sort_by', '')
 
-    visits = Visit.objects.all()
-    visits = visits.order_by('-date')
-
-    if sort_by == 'first_name':
-        visits = visits.order_by('user__first_name')
-    elif sort_by == 'last_name':
-        visits = visits.order_by('user__last_name')
-    elif sort_by == 'date':
-        visits = visits.order_by('date')
-    elif sort_by == 'arrival_time':
-        visits = visits.order_by('arrival_time')
-    elif sort_by == 'living_time':
-        visits = visits.order_by('leaving_time')
-    elif sort_by == 'working_time':
-        visits = visits.order_by('working_time')
-
-    if search_query:
-        visits = visits.filter(
-            Q(user__first_name__icontains=search_query) |
-            Q(user__last_name__icontains=search_query) |
-            Q(date__icontains=search_query)
-        )
-
-    if since_date_filter!='':
-        visits = visits.filter(date__gte=since_date_filter)
-        if before_date_filter!='':
-            visits = visits.filter(date__gte=since_date_filter, date__lte=before_date_filter)
-        else:
-            visits = visits.filter(date=since_date_filter)
-    else:
-        if before_date_filter != '':
-            visits = visits.filter(date__lte=before_date_filter)
-    if form.is_valid():
-        selected_user = form.cleaned_data['users']
-        if selected_user:
-            visits = visits.filter(user=selected_user)
+    visits = get_filtered_visits(request, search_query, since_date_filter, before_date_filter, sort_by)
 
     if request.GET.get('download'):
-        if search_query:  # Применяем фильтрацию поиска только если есть параметр поиска
-            visits = visits.filter(
-                Q(user__first_name__icontains=search_query) |
-                Q(date__icontains=search_query)
-            )
-
-        # Создаем новый файл Excel и добавляем данные
-        wb = Workbook()
-        ws = wb.active
-        headers = ['Фамилия', 'Имя', 'Время прибытия', 'Время ухода', 'Время работы', 'Время опоздания', 'Переработки']
-        ws.append(headers)
-        for obj in visits:
-            row = [obj.user.last_name, obj.user.first_name, obj.arrival_time, obj.leaving_time, obj.working_time,
-                   obj.lateness, obj.recycling]
-            ws.append(row)
-
-        # Генерируем файл Excel в памяти и создаем HTTP-ответ для скачивания файла
-        file_stream = BytesIO()
-        wb.save(file_stream)
-        file_stream.seek(0)
-
-        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = 'attachment; filename=report.xlsx'
-        response.write(file_stream.read())
-        return response
+        return download_visits_report(visits, search_query)
 
     context = {
         'visits': visits,
@@ -180,5 +64,9 @@ def ceramic_admin_view(request):
         'sort_by': sort_by
     }
     return render(request, 'loginapp/admin.html', context)
+
+
+
+
 
 
