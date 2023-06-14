0c9096cfc1be328b5a253985e8d69cebec12498a
DarthZiel <Studerzhan@gmail.com>
2023-06-14 15:19:08 +0600

    customise admin

--- loginapp/admin.py
+++ loginapp/admin.py
@@ -1,25 +1,38 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User, Group
-from .models import Visit, UniqueLink
+from .models import Visit
+
 
 
 class CustomUserAdmin(UserAdmin):
+    verbose_name_plural = "Мои пользователи и группы"
     list_display = ['username', 'first_name', 'last_name', 'last_login']
     list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
-
+#Сотрудники
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
-
 
 class CustomVisitAdmin(admin.ModelAdmin):
     readonly_fields = ['date', 'arrival_time', 'leaving_time', 'working_time', 'lateness', 'recycling']
 
 
+
 admin.site.register(Visit, CustomVisitAdmin)
-admin.site.register(UniqueLink)
+
+
 admin.site.unregister(Group)
+
+
+
+
+
 
 admin.site.index_title = "Добро пожаловать в интерфейс администратора Ceramic Pro!"
 
